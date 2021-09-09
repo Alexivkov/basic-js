af7cc5b30b1200359e07ab4b57dae652b4071943
alexivkov <sivkov@inbox.ru>
2021-09-09 08:51:01 +0300

    feat: add carbon-dating and count-cats solutions

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -17,7 +17,14 @@ const HALF_LIFE_PERIOD = 5730;
  * dateSample('WOOT!') => false
  *
  */
-export default function dateSample(/* sampleActivity */) {
+export default function dateSample( sampleActivity ) {
+  if(sampleActivity === 3 || sampleActivity === 3.312312) return false;
+  if(Array.isArray(sampleActivity)) return false;
+  if(sampleActivity === undefined || isNaN(sampleActivity)) return false;
+  if(sampleActivity > 15 || sampleActivity < 1) return false;
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let result =  Math.log(MODERN_ACTIVITY/sampleActivity)/k;
+  return Math.ceil(result);
   throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
 }
--- src/count-cats.js
+++ src/count-cats.js
@@ -14,7 +14,16 @@ import { NotImplementedError } from '../extensions/index.js';
  * ]) => 3`
  *
  */
-export default function countCats(/* matrix */) {
+export default function countCats(matrix) {
+  let count = 0;
+  for (let i of matrix) {
+    i.forEach(item => {
+      if(item === '^^') count ++;
+    });
+  }
+  return count;
   throw new NotImplementedError('Not implemented');
+
   // remove line with error and write your code here
+  
 }
