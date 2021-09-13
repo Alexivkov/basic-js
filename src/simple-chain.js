c241acb88af0a3f7071e7de176b3d02c36ba4ec1
alexivkov <sivkov@inbox.ru>
2021-09-13 08:20:11 +0300

    feat:add simple-chain function

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,25 +6,35 @@ import { NotImplementedError } from '../extensions/index.js';
  */
 export default {
   getLength() {
-
+    return this.value.length;
     throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
   },
   addLink(value) {
-    
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-    
+    if (!this.value) this.value = [`( ${value} )`];
+    else this.value.push(`( ${value} )`);
+    return this;
+    throw new NotImplementedError('Not implemented');    
   },
-  removeLink(/* position */) {
+  removeLink(position) {
+        if (position < 1 || isNaN(position) || position > this.value.length) {
+      this.value = [];
+      throw new Error("You can\'t remove incorrect link!");
+    }
+    this.value.splice(position - 1,1);
+    return this;
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   reverseChain() {
+    this.value.reverse();
+    return this;
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   },
   finishChain() {
+    let a = this.value.slice();
+    this.value = [];
+    return a.join('~~');
     throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
